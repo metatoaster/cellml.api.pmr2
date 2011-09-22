981a8524b57021332dc6e48cffd5c032d6fe4153
Tommy Yu <tommy.yu@auckland.ac.nz>
2011-09-22 18:23:37 +1200

    It wasn't really a "singleton"
      - Now it is, across all instances.
      - The original implementation would be an instanced property.
      - Test cases for both.
      - Delete is supported (to allow them to be reinitialized).

--- cellml/api/pmr2/property.py
+++ cellml/api/pmr2/property.py
@@ -1,19 +1,80 @@
-class singleton_property(object):
-    """\
-    Instantiate a single instance of this property for this instance.
-    """
+# perhaps we could inherit from property instead of object?
+# Obvious none of this is considered thread-safe.
+
+class base_property(object):
 
     def __init__(self, method):
         self.method = method
+
+    @property
+    def name(self):
+        return '_%s' % self.method.__name__
+
+
+class instance_property(base_property):
+    """\
+    Property decorator that automate caching of the first result.
+
+    This decorator checks whether there was a result that was cached by
+    the parent object, if not, calls the property method, cache it then
+    return the cached result.
+    """
 
     def __get__(self, obj, objtype):
         """\
         Assign the outout of the met
         """
 
-        name = '_%s' % self.method.__name__
+        name = self.name
         result = getattr(obj, name, None)
-        if not result:
+        if result is None:
             result = self.method(obj)
             setattr(obj, name, result)
         return result
+
+    def __delete__(self, obj):
+        """\
+        Delete the property.
+        """
+        
+        name = self.name
+        result = getattr(obj, name, None)
+        if result is None:
+            return
+        delattr(obj, name)
+
+
+class singleton_property(base_property):
+    """\
+    Property decorator that automate caching of the first result in this
+    decorator instance.
+
+    Basically this instantiate a single instance of the property 
+    referenced by this class into this decorator instance, thus all
+    subsequent attempts will return this instance, functionally 
+    equivalent to a singleton that is the same across all instances of
+    the object.
+    """
+
+    def __get__(self, obj, objtype):
+        """\
+        Assign the outout of the met
+        """
+
+        name = self.name
+        result = getattr(self, name, None)
+        if result is None:
+            result = self.method(obj)
+            setattr(self, name, result)
+        return result
+
+    def __delete__(self, obj):
+        """\
+        Delete the property.
+        """
+        
+        name = self.name
+        result = getattr(self, name, None)
+        if result is None:
+            return
+        delattr(self, name)
--- cellml/api/pmr2/tests/test_property.py
+++ cellml/api/pmr2/tests/test_property.py
@@ -3,7 +3,29 @@ import unittest
 from cellml.api.pmr2.property import *
 
 
-class TestClass(object):
+class InstanceTestClass(object):
+    counter = None
+
+    def __init__(self):
+        self.counter = 0
+        self.items = None
+
+    @instance_property
+    def test_prop(self):
+        self.counter += 1
+        return self.counter
+
+    @instance_property
+    def test_items(self):
+        items = []
+        return items
+
+    @instance_property
+    def test_nested(self):
+        return self.test_prop * 1
+
+
+class SingletonTestClass(object):
     counter = None
 
     def __init__(self):
@@ -20,8 +42,12 @@ class TestClass(object):
         items = []
         return items
 
+    @singleton_property
+    def test_nested(self):
+        return self.test_prop * 1
 
-class PropertyTestCase(unittest.TestCase):
+
+class InstancePropertyTestCase(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -31,48 +57,147 @@ class PropertyTestCase(unittest.TestCase):
 
     def test_0000_side_effect_once(self):
         # to test this side effect is only triggered once
-        tester = TestClass()
+        tester = InstanceTestClass()
         value1 = tester.test_prop
         value2 = tester.test_prop
 
-        self.assertEqual(value1, value2)
         self.assert_(isinstance(tester.test_prop, int))
+        self.assertEqual(tester.counter, value1)
+        self.assertEqual(tester.counter, value2)
 
     def test_0001_multiple_instances(self):
         # to test this side effect is only triggered once, even with
         # multiple instances.
-        tester1 = TestClass()
+        tester1 = InstanceTestClass()
         value1 = tester1.test_prop
         value2 = tester1.test_prop
 
-        tester2 = TestClass()
+        tester2 = InstanceTestClass()
         value3 = tester2.test_prop
         value4 = tester2.test_prop
 
+        self.assertEqual(tester1.counter, value1)
         self.assertEqual(value1, value2)
         self.assertEqual(value1, value3)
         self.assertEqual(value1, value4)
 
-    def test_0002_multiple_instances_instanced_attirbutes(self):
+    def test_0002_multiple_instances_instanced_attributes(self):
         # Make sure the different instances have unique items.
 
-        tester1 = TestClass()
+        tester1 = InstanceTestClass()
+
         tester1.test_items.append(1)
         tester1.test_items.append(2)
 
-        tester2 = TestClass()
+        tester2 = InstanceTestClass()
         tester2.test_items.append(3)
         tester2.test_items.append(4)
 
         self.assertEqual(tester1.test_items, [1, 2])
         self.assertEqual(tester2.test_items, [3, 4])
 
+        # since this property creates a local instance.
         self.assertEqual(tester2._test_items, tester2.test_items)
+
+    def test_0003_empty_not_multiple(self):
+        # Make sure empty instances are not recreated on every access.
+
+        tester = InstanceTestClass()
+        self.assertEqual(id(tester.test_items), id(tester.test_items))
+
+    def test_0004_delete(self):
+
+        tester = InstanceTestClass()
+        tester.test_items.append(1)
+        del tester.test_items
+        self.assertEqual(tester.test_items, [])
+
+
+class SingletonPropertyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_0000_side_effect_once(self):
+        # to test this side effect is only triggered once
+        tester = SingletonTestClass()
+        value1 = tester.test_prop
+        value2 = tester.test_prop
+
+        self.assert_(isinstance(tester.test_prop, int))
+        self.assertEqual(tester.counter, value1)
+        self.assertEqual(tester.counter, value2)
+
+    def test_0001_multiple_instances(self):
+        # to test this side effect is only triggered once, even with
+        # multiple instances.
+        tester1 = SingletonTestClass()
+        value1 = tester1.test_prop
+        value2 = tester1.test_prop
+
+        tester2 = SingletonTestClass()
+        value3 = tester2.test_prop
+        value4 = tester2.test_prop
+
+        # They won't be incremented because the decorator instance 
+        # already contain the value.
+        self.assertEqual(tester1.counter, 0)
+        self.assertEqual(tester2.counter, 0)
+
+        self.assertEqual(value1, 1)
+        self.assertEqual(value1, value2)
+        self.assertEqual(value1, value3)
+        self.assertEqual(value1, value4)
+
+    def test_0002_multiple_instances_instanced_attributes(self):
+        # Make sure the different instances all share the same 
+        # singleton attribute.
+
+        tester1 = SingletonTestClass()
+        tester2 = SingletonTestClass()
+
+        # They also have to be the same.
+        self.assertEqual(id(tester1.test_items), id(tester2.test_items))
+
+        tester1.test_items.append(1)
+        tester1.test_items.append(2)
+
+        tester2.test_items.append(3)
+        tester2.test_items.append(4)
+
+        self.assertEqual(tester1.test_items, [1, 2, 3, 4])
+        self.assertEqual(tester2.test_items, [1, 2, 3, 4])
+
+    def test_0003_persisted_singleton(self):
+        # Since this is still within the same thread of execution,
+        # the values from test_0002 should persist also.
+
+        tester = SingletonTestClass()
+        tester.test_items.append(5)
+        tester.test_items.append(6)
+
+        self.assertEqual(tester.test_items, [1, 2, 3, 4, 5, 6])
+
+    def test_0004_delete_singleton(self):
+        # still can be deleted however to allow reinstantiation.
+
+        tester = SingletonTestClass()
+        self.assertEqual(tester.test_items, [1, 2, 3, 4, 5, 6])
+        del tester.test_items
+        tester.test_items.append(7)
+        tester.test_items.append(8)
+
+        self.assertEqual(tester.test_items, [7, 8])
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
-    suite.addTest(makeSuite(PropertyTestCase))
+    suite.addTest(makeSuite(SingletonPropertyTestCase))
+    suite.addTest(makeSuite(InstancePropertyTestCase))
     return suite
 
 if __name__ == '__main__':
